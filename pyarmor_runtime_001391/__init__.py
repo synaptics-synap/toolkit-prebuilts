36e5ab85873e154efb83fcf975df1ca87dca7f46
meepat <MeetDineshbhai.Patel@synaptics.com>
2024-08-29 13:14:53 -0700

    Obfuscate Acuitylib scripts
    -Obfuscate scripts for python3.10 and python3.12 using pyarmor
    -Merge to one Acuitylib scripts making it compatible for both python versions

--- pyarmor_runtime_001391/__init__.py
+++ pyarmor_runtime_001391/__init__.py
@@ -1,2 +1,3 @@
-# Pyarmor 8.5.8 (basic), 001391, 2024-06-06T06:27:47.905988
-from .pyarmor_runtime import __pyarmor__
+# Pyarmor 8.5.11 (basic), 001391, 2024-08-28T13:27:14.489427
+from sys import version_info as py_version
+__pyarmor__ = __import__("py%d%d.pyarmor_runtime" % py_version[:2], globals(), locals(), ["__pyarmor__"], 1).__pyarmor__
